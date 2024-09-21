37da5c71efbd77ef879adad23b681f15cdb5d1d6
yejiscore <yejisong24@gmail.com>
2024-09-21 21:41:47 +0900

    Initial Commit

---
+++ 1. 스프링입문/hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
@@ -0,0 +1,22 @@
+package hello.hello_spring;
+
+import hello.hello_spring.aop.TimeTraceAop;
+import hello.hello_spring.repository.*;
+import hello.hello_spring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+}
---
+++ 1. 스프링입문/hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,48 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent( m -> {
+            throw new IllegalThreadStateException("이미 존재하는 회원");
+        });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * id로 회원 조회
+     */
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
